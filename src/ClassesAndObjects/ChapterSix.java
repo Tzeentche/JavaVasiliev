23c6d4fb6f9680f46c10dd0eac536830c8a8e948
Tzeentche <ilyasugako87@gmail.com>
2019-07-22 23:36:30 +0300

    Added sample from Chapter 6

---
+++ src/ClassesAndObjects/ChapterSix.java
@@ -0,0 +1,30 @@
+package ClassesAndObjects;
+
+import java.util.*;
+import javax.swing.*;
+
+class MyClass{
+
+    int num;
+    char symb;
+    String text;
+}
+
+public class ChapterSix {
+
+    public static void main(String[] args) {
+
+        MyClass obj = new MyClass();
+
+        obj.num = 12;
+        obj.symb = 'A';
+        obj.text = "Text line";
+
+        String str = "Values of object's fields: \n";
+        str += " num = " + obj.num + "\n";
+        str += " symb = " + obj.symb + " \n";
+        str += " text = " + obj.text + " \n";
+
+        JOptionPane.showMessageDialog(null, str);
+    }
+}
