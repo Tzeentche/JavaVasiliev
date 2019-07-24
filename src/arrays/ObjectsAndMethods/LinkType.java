4c06e809c8c45f6374174461533554dd0f6ea8e1
Tzeentche <ilyasugako87@gmail.com>
2019-07-24 23:45:14 +0300

    Started development decision for chapter 7: use link type

---
+++ src/arrays/ObjectsAndMethods/LinkType.java
@@ -0,0 +1,25 @@
+package arrays.ObjectsAndMethods;
+
+import javax.swing.*;
+
+class SeventhClass {
+
+    String name;
+
+    SeventhClass(String name) {
+
+        this.name = name;
+    }
+
+    void show() {
+
+        System.out.println("Object with name \"" + name + "\".");
+    }
+}
+public class LinkType {
+
+    static void ChangeRef(SeventhClass obj){
+
+       
+    }
+}
