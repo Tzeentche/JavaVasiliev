ae27d690b8b596737a7e17ba1808082e5d1832eb
Tzeentche <ilyasugako87@gmail.com>
2019-07-25 23:14:18 +0300

    Added decision for sample Inner Classes (Vasiliev, chapter 7)

--- src/arrays/ObjectsAndMethods/InnerClasses.java
+++ src/arrays/ObjectsAndMethods/InnerClasses.java
@@ -1,4 +1,66 @@
 package arrays.ObjectsAndMethods;
 
+import javax.swing.*;
+
+class TheBig {
+
+    String name;
+    TheSmall ID;
+
+    TheBig(String name,int n) {
+
+        this.name = name;
+
+        ID = new TheSmall(n);
+
+        ID.show();
+    }
+
+    String getName() {
+
+        String txt = "Name of object: " + name + " \n";
+        return txt;
+    }
+
+    class TheSmall {
+
+        int[] code;
+
+        TheSmall(int n) {
+
+            code = new int[n];
+
+            for (int i = 0; i < code.length; i ++) {
+
+                code[i] = (int)(10 * Math.random());
+            }
+        }
+
+        String getCode() {
+
+            String txt = "Object's code: |";
+
+            for (int i = 0;i<code.length; i ++) {
+
+                txt += code[i] + " | ";
+            }
+
+            return txt;
+        }
+
+        void show(){
+
+            String txt = getName() + getCode();
+
+            JOptionPane.showMessageDialog(null, txt);
+        }
+    }
+}
+
 public class InnerClasses {
+
+    public static void main(String[] args) {
+
+        new TheBig("Big", 7);
+    }
 }
