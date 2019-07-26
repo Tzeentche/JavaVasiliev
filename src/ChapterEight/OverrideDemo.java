7eb6488cb3c63bd696025900e31b3d466ddffbe4
Tzeentche <ilyasugako87@gmail.com>
2019-07-26 23:51:19 +0300

    Added main class and SubClass for sample Override Methods (Vasiliev, chapter 8)

--- src/ChapterEight/OverrideDemo.java
+++ src/ChapterEight/OverrideDemo.java
@@ -1,4 +1,42 @@
 package ChapterEight;
 
+import javax.swing.*;
+
+class ATwo {
+
+    int first;
+
+    void set() {
+
+        first = 0;
+        System.out.println("Zero value of field.");
+    }
+
+    void set(int n) {
+
+        first = n;
+        show();
+    }
+
+    void show() {
+
+        System.out.println("Pole " + first + ".");
+    }
+
+    void showAll() {
+
+        System.out.println("All fields - to the screen!");
+        show();
+    }
+}
+
+class BTwo extends ATwo {
+
+    int second;
+    void set(int n) {
+
+        
+    }
+}
 public class OverrideDemo {
 }
