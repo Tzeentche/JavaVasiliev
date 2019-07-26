d445907785e8ab0df75cc5be9ffae93c8d7f7635
Tzeentche <ilyasugako87@gmail.com>
2019-07-26 23:41:49 +0300

    Finished developing for sample Sub Contructor (Vasiliev, chapter 8)

--- src/ChapterEight/SubConstrDemo.java
+++ src/ChapterEight/SubConstrDemo.java
@@ -1,4 +1,79 @@
 package ChapterEight;
 
+import javax.swing.*;
+
+class A {
+
+    int number;
+    char symbol;
+
+    A() {
+
+        number = 0;
+        symbol = 'A';
+        System.out.println("Constructor without arguments!");
+        show();
+    }
+
+    A(int n) {
+
+        number = n;
+        symbol = 'B';
+
+        System.out.println("Constructor with one argument!");
+        show();
+    }
+
+    A(int n, char s) {
+
+        number = n;
+        symbol = s;
+        System.out.println("Constructor with two arguments!");
+        show();
+    }
+
+    void show() {
+
+        System.out.println("Fields " + number + " and " + symbol + " . ");
+    }
+}
+
+class B extends A {
+
+    String text;
+
+    B() {
+
+        super();
+        text = "Without arguments";
+        showText();
+    }
+
+    B(String txt) {
+
+        super(txt.length());
+        text = txt;
+        showText();
+    }
+
+    B(int n, char s, String txt) {
+
+        super(n, s);
+        text = txt;
+        showText();
+    }
+
+    void showText() {
+
+        System.out.println("Text field \"" + text + "\" .");
+    }
+}
 public class SubConstrDemo {
+
+    public static void main(String[] args) {
+
+        new B();
+        new B("One argument");
+        new B(100, 'F', "Three arguments");
+    }
 }
