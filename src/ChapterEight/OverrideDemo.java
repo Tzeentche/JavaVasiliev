41c4231a5f6d5e441de7e4e45fec7b33f9cae2ea
Tzeentche <ilyasugako87@gmail.com>
2019-07-26 23:56:44 +0300

    Finished developing for sample Override Methods (Vasiliev, chapter 8)

--- src/ChapterEight/OverrideDemo.java
+++ src/ChapterEight/OverrideDemo.java
@@ -35,8 +35,42 @@ class BTwo extends ATwo {
     int second;
     void set(int n) {
 
-        
+        first = n;
+        second = n;
+
+        show();
+    }
+
+    void set(int m, int n) {
+
+        second = n;
+
+        super.set(m);
+    }
+
+    void show() {
+
+        System.out.println("Fields " + first + " and " + second + ".");
     }
 }
+
 public class OverrideDemo {
+
+    public static void main(String[] args) {
+
+        ATwo objA = new ATwo();
+        objA.set();
+        objA.set(100);
+
+        BTwo objB = new BTwo();
+
+        objB.set();
+        objB.second = -1;
+        objB.show();
+        objB.set(200);
+        objB.set(1, 2);
+
+        objA.showAll();
+        objB.showAll();
+    }
 }
