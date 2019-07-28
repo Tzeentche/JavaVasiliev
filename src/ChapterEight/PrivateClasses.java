f50855b3c8bef8f61586c5e3cdb1775ad4357f28
Tzeentche <ilyasugako87@gmail.com>
2019-07-28 21:40:06 +0300

    Finished developing sample Private Classes (Vasiliev, chapter 8)

--- src/ChapterEight/PrivateClasses.java
+++ src/ChapterEight/PrivateClasses.java
@@ -2,6 +2,51 @@ package ChapterEight;
 
 import javax.swing.*;
 
+class Base {
+
+    private String name;
+    private int number;
+
+    Base(String name, int number) {
+
+        this.name = name;
+        this.number = number;
+    }
+
+    private void show() {
+
+        System.out.println("Object with name " + name);
+    }
+
+    void showAll() {
+
+        show();
+        System.out.println("Numeric field of object equals " + number);
+    }
+
+    void setAll(String name, int number) {
+
+        this.name = name;
+        this.number = number;
+    }
+}
+
+class SubBase extends Base {
+
+    SubBase(String str, int num){
+
+        super(str,num);
+        showAll();
+    }
+}
 
 public class PrivateClasses {
+
+    public static void main(String[] args) {
+
+        SubBase obj = new SubBase("NEW", 1);
+        obj.setAll("EQUALS", 2);
+
+        obj.showAll();
+    }
 }
