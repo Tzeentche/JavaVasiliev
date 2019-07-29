ae35dbda483b2976be7285ecd0a82aa179635225
Tzeentche <ilyasugako87@gmail.com>
2019-07-29 23:49:58 +0300

    Finished developing decision for sample Interface variable (Vasiliev, chapter 8)

--- src/ChapterEight/InterfaceRefDemo.java
+++ src/ChapterEight/InterfaceRefDemo.java
@@ -2,5 +2,37 @@ package ChapterEight;
 
 import javax.swing.*;
 
+interface BaseThird {
+
+    void show();
+}
+
+class A implements BaseThird {
+
+    public void show() {
+
+        System.out.println("It is method of A-class!");
+    }
+}
+
+class B implements BaseThird {
+
+    public void show() {
+
+        System.out.println("It is method of B-class!");
+    }
+}
+
 public class InterfaceRefDemo {
+
+    public static void main(String[] args) {
+
+        BaseThird ref;
+
+        ref = new A();
+        ref.show();
+
+        ref = new B();
+        ref.show();
+    }
 }
