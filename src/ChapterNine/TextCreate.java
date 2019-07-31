24c8c3b5a7cbbaa537e5aac6adac23bfa0abd2e6
Tzeentche <ilyasugako87@gmail.com>
2019-07-31 23:18:16 +0300

    Finishing create decision for sample Text Creating (Vasiliev, chapter 9)

--- src/ChapterNine/TextCreate.java
+++ src/ChapterNine/TextCreate.java
@@ -3,6 +3,22 @@ package ChapterNine;
 public class TextCreate {
 
     public static void main(String[] args) {
-         
+
+        String str1 = new String("Text literal");
+        System.out.println(str1);
+
+        String str2 = new String(str1);
+        System.out.println(str2);
+
+        String str3 = new String();
+        System.out.println(str3);
+
+        char[] symbs = new char[] {'A','r','r','a','y'};
+        String str4 = new String(symbs);
+        System.out.println(str4);
+
+        byte[] nums = new byte[] {78, 97, 109, 98, 101, 114, 115};
+        String str5 = new String(nums);
+        System.out.println(str5);
     }
 }
