e2a2ac6a7ccf8d195435c05215520865d5d79008
Tzeentche <ilyasugako87@gmail.com>
2019-07-31 23:57:22 +0300

    Finished create decision for sample String Tokenizer (Vasiliev, chapter 9)

--- src/ChapterNine/StringTokenizer.java
+++ src/ChapterNine/StringTokenizer.java
@@ -1,4 +1,22 @@
 package ChapterNine;
 
+import java.util.*;
+
 public class StringTokenizer {
+
+    public static void main(String[] args) {
+
+        String text = "Wery simple text: contains with words< without numbers!";
+
+        StringTokenizer str = new StringTokenizer(text);
+
+        String s;
+
+        while(str.hasMoreTokens()) {
+
+            s = str.nextToken();
+
+            System.out.println(s);
+        }
+    }
 }
