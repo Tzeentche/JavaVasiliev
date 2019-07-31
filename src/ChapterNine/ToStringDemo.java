408aed7e5a8c02806962f82db15a053f38553962
Tzeentche <ilyasugako87@gmail.com>
2019-07-31 23:42:25 +0300

    Finishing create decision for sample Method Override (Vasiliev, chapter 9)

--- src/ChapterNine/ToStringDemo.java
+++ src/ChapterNine/ToStringDemo.java
@@ -1,4 +1,49 @@
 package ChapterNine;
 
+class MyOverrideClass {
+
+    char[] code;
+    int count;
+    String name;
+
+    MyOverrideClass(char[] code, int count, String name) {
+
+        int i;
+
+        this.code = new char[code.length];
+
+        for(i = 0; i < code.length; i++) {
+
+            this.code[i] = code[i];
+        }
+
+        this.count = count;
+
+        this.name = new String(name);
+    }
+
+    public String toString() {
+
+        String text = "Values of fields object. \n";
+
+        text += "Field \"NAME\": " + name + "\n";
+        text += "Field \"COUNT\": " + count + "\n";
+        text += "Field \"CODE\": |";
+
+        for (int i = 0; i < code.length; i++) {
+
+            text += " " + code[i] + " |";
+        }
+
+        return text;
+    }
+}
+
 public class ToStringDemo {
+
+    public static void main(String[] args) {
+
+        MyOverrideClass obj = new MyOverrideClass(new char[] {'Z', 'A', 'R', 'Q', 'W'}, 100, "NEW");
+        System.out.println(obj);
+    }
 }
