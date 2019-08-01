a981294730b52eba7450c7d6650c0cc6f25eaa1f
Tzeentche <ilyasugako87@gmail.com>
2019-08-01 23:31:17 +0300

    Started create decision for sample Date (Vasiliev, chapter 9)

--- src/ChapterNine/StringTokenizer.java
+++ src/ChapterNine/StringTokenizer.java
@@ -1,22 +1,23 @@
-package ChapterNine;
-
-import java.util.*;
-
-public class StringTokenizer {
-
-    public static void main(String[] args) {
-
-        String text = "Wery simple text: contains with words< without numbers!";
-
-        StringTokenizer str = new StringTokenizer(text);
-
-        String s;
-
-        while(str.hasMoreTokens()) {
-
-            s = str.nextToken();
-
-            System.out.println(s);
-        }
-    }
-}
+//package ChapterNine;
+//
+//import java.util.*;
+//import java.lang.*;
+//
+//public class StringTokenizer {
+//
+//    public static void main(String[] args) {
+//
+//        String text = "Very simple text: contains with words, without numbers!";
+//
+//        StringTokenizer str = new StringTokenizer(text);
+//
+//        String s;
+//
+//        while(str.hasMoreTokens()) {
+//
+//            s = str.nextToken();
+//
+//            System.out.println(s);
+//        }
+//    }
+//}
