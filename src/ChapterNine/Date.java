29d0ac2559350f88d05028a4c807bd5831bb469b
Tzeentche <ilyasugako87@gmail.com>
2019-08-01 23:34:43 +0300

    Finished create decision for sample Date (Vasiliev, chapter 9)

--- src/ChapterNine/Date.java
+++ src/ChapterNine/Date.java
@@ -1,4 +1,15 @@
 package ChapterNine;
 
+import java.util.*;
+
 public class Date {
+
+    public static void main(String[] args) {
+
+        Date now = new Date();
+        Date old = new Date();
+
+        System.out.println("Actual date and time: " + now);
+        System.out.println("One more date: " + old);
+    }
 }
