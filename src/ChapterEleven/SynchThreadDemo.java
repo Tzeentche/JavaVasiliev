b7fe4a34613b2fbf9d8f4b2b1dd0e2831074baf8
Tzeentche <ilyasugako87@gmail.com>
2019-08-03 20:39:21 +0300

    Finished create decision for sample Threads Synchronizing (Vasiliev, chapter 11)

--- src/ChapterEleven/SynchThreadDemo.java
+++ src/ChapterEleven/SynchThreadDemo.java
@@ -1,4 +1,95 @@
 package ChapterEleven;
 
+import java.util.*;
+
+class MySynchClass {
+
+    private int count;
+
+    MySynchClass() {
+
+        count = 0;
+    }
+
+    void set(int count, String name) {
+
+        this.count = count;
+        System.out.println(name + "Set the field's value " + count + ": " + new Date());
+    }
+
+    int get(String name) {
+
+        System.out.println(name + "Field's value read's " + count + ": " + new Date());
+
+        return count;
+    }
+}
+
+class MySynchThread extends Thread {
+
+    private boolean UpDown;
+    private String name;
+    private MySynchClass obj;
+
+    public void run() {
+
+        Random rnd = new Random();
+
+        int number;
+
+        for (int i = 1; i <= 3; i ++) {
+
+            synchronized(obj) {
+                try {
+
+                    number = obj.get(name);
+
+                    if (UpDown) number++;
+
+                    else number--;
+
+                    sleep(1000 + rnd.nextInt(9000));
+
+                    obj.set(number, name);
+
+                } catch (InterruptedException e) {
+
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+
+    MySynchThread(boolean UpDown, MySynchClass obj) {
+
+        this.UpDown = UpDown;
+
+        if(UpDown) name = "Up-thread. ";
+
+        else name = "Down-tread. ";
+
+        this.obj = obj;
+
+        start();
+    }
+}
+
 public class SynchThreadDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MySynchClass obj = new MySynchClass();
+
+        MySynchThread thA = new MySynchThread(true, obj);
+
+        MySynchThread thB = new MySynchThread(false, obj);
+
+        thA.join();
+
+        thB.join();
+
+        System.out.println("Finally: ");
+
+        obj.get(" ");
+    }
 }
