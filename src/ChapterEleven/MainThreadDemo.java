8c074c64a93fbba06c8303a98ddbfd2a0865c111
Tzeentche <ilyasugako87@gmail.com>
2019-08-03 18:47:35 +0300

    Finished create decision for sample Main Thread (Vasiliev, chapter 11)

--- src/ChapterEleven/MainThreadDemo.java
+++ src/ChapterEleven/MainThreadDemo.java
@@ -2,7 +2,27 @@ package ChapterEleven;
 
 public class MainThreadDemo {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t;
+        long time = 2500;
+
+        t = Thread.currentThread();
+
+        System.out.println(t);
+
+        t.setName("It is a \"mainest\" thread!");
+
+        int p = t.getPriority();
+
+        t.setPriority(++p);
+
+        System.out.println(t);
+
+        System.out.println("Thread's running has stopped on " + (double)time / 1000 + " seconds.");
+
+        Thread.sleep(time);
+
+        System.out.println("Program's work stopped!");
     }
 }
