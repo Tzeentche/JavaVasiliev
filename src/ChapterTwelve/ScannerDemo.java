fec2857c2f7180ce583c37e3cfd899ff3c7cdb14
Tzeentche <ilyasugako87@gmail.com>
2019-08-04 21:34:46 +0300

    Finished create decision for sample Scanner From Keyboard (Vasiliev, chapter 12)

--- src/ChapterTwelve/ScannerDemo.java
+++ src/ChapterTwelve/ScannerDemo.java
@@ -3,5 +3,28 @@ package ChapterTwelve;
 import java.util.*;
 
 public class ScannerDemo {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String name;
+        int age;
+        double weight;
+
+        System.out.println("What is your name?");
+
+        name = input.nextLine();
+        System.out.println("Good morning, " + name + "!");
+        System.out.println("How old are you? ");
+
+        age = input.nextInt();
+
+        System.out.println("You are " + age + "years old!");
+        System.out.println("Enter your weight (in kg.): ");
+
+        weight = input.nextDouble();
+
+        System.out.println("Your weight is " + weight +"kg.");
+    }
 }
