442d01d0e59f766fd7664361edc55ab83c92dcd2
Tzeentche <ilyasugako87@gmail.com>
2019-08-04 23:43:33 +0300

    Finished create decision for sample Input / Output (Vasiliev, chapter 12)

--- src/ChapterTwelve/FileInputOutputDemo.java
+++ src/ChapterTwelve/FileInputOutputDemo.java
@@ -12,7 +12,38 @@ public class FileInputOutputDemo {
 
         try {
 
-            
+            FileInputStream fin = new FileInputStream(FilePath);
+
+            FileOutputStream fout = new FileOutputStream("D:/Books/Files/result.txt");
+
+            int s = fin.read();
+
+            while(s != - 1) {
+
+                switch(s) {
+
+                    case 'T':
+                    s ='t';
+                    break;
+
+                    case '.':
+                        fout.write('<');
+                        fout.write('*');
+                        s ='>';
+                        break;
+                }
+
+                fout.write(s);
+                s = fin.read();
+            }
+
+            JOptionPane.showMessageDialog(null, "Changes in file get's!", "Program's work is end", JOptionPane.INFORMATION_MESSAGE);
+
+            fin.close();
+            fout.close();
+        } catch(FileNotFoundException e) {
+
+            JOptionPane.showMessageDialog(null, "That's file wasn't!", "It's mistake!", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
