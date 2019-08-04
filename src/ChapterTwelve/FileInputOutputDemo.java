2aac13c5c56c26d8e0d33222646d32f92ada06f0
Tzeentche <ilyasugako87@gmail.com>
2019-08-04 23:35:28 +0300

    Started create decision for sample Input / Output (Vasiliev, chapter 12)

---
+++ src/ChapterTwelve/FileInputOutputDemo.java
@@ -0,0 +1,18 @@
+package ChapterTwelve;
+
+import java.io.*;
+import javax.swing.*;
+
+public class FileInputOutputDemo {
+
+    public static void main(String[] args) throws IOException {
+
+        String FilePath = "D:/Books/Files";
+        FilePath += JOptionPane.showInputDialog("Set thefile'sname");
+
+        try {
+
+            
+        }
+    }
+}
