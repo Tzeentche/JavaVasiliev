c47ef08f190b7dc9391aaa1b1139bf67a4b614ba
Tzeentche <ilyasugako87@gmail.com>
2019-08-05 23:27:51 +0300

    Started dveloping decision for sample Window With Text Labele Two (Vasiliev, chapter 13)

---
+++ src/ChapterThirteen/FrameAndLabelTwo.java
@@ -0,0 +1,11 @@
+package ChapterThirteen;
+
+import javax.swing.*;
+
+public class FrameAndLabelTwo {
+
+    public static void main(String[] args) {
+
+        new MyFrameTwo(" Label with pict!", 300, 400);
+    }
+}
