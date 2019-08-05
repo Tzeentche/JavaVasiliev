240b3df0af3b9f329088fdb03e2e3bc517494112
Tzeentche <ilyasugako87@gmail.com>
2019-08-05 23:28:12 +0300

    Finished dveloping decision for sample Window With Text Labele Two (Vasiliev, chapter 13)

--- src/ChapterThirteen/FrameAndLabelTwo.java
+++ src/ChapterThirteen/FrameAndLabelTwo.java
@@ -2,6 +2,24 @@ package ChapterThirteen;
 
 import javax.swing.*;
 
+class MyFrameTwo {
+
+    MyFrameTwo(String text, int x, int y) {
+
+        JFrame frm = new JFrame("Window with label");
+
+        frm.setBounds(x, y, 250, 120);
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon icn = new ImageIcon("d:/Books/Java_guide/pictures/redshd.gif");
+        JLabel lbl = new JLabel(text, icn, JLabel.CENTER);
+
+        frm.add(lbl);
+
+        frm.setVisible(true);
+    }
+}
+
 public class FrameAndLabelTwo {
 
     public static void main(String[] args) {
