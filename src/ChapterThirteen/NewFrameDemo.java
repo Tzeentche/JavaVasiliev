9b411f14e3f6b9da1810f5f1407928e652b61396
Tzeentche <ilyasugako87@gmail.com>
2019-08-05 23:14:12 +0300

    Finished dveloping decision for sample Simple Frame (Vasiliev, chapter 13)

--- src/ChapterThirteen/NewFrameDemo.java
+++ src/ChapterThirteen/NewFrameDemo.java
@@ -1,4 +1,24 @@
 package ChapterThirteen;
 
+import javax.swing.*;
+
+class NewFrame {
+
+    NewFrame() {
+
+
+        JFrame MyFrame = new JFrame("New window");
+
+        MyFrame.setSize(300, 200);
+
+        MyFrame.setVisible(true);
+    }
+}
+
 public class NewFrameDemo {
+
+    public static void main(String[] args) {
+
+        new NewFrame();
+    }
 }
