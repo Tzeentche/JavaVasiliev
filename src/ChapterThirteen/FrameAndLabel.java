752413db7e1a5dcb23cb1af3505ada009683493b
Tzeentche <ilyasugako87@gmail.com>
2019-08-05 23:20:12 +0300

    Finished dveloping decision for sample Window With Text Lable (Vasiliev, chapter 13)

--- src/ChapterThirteen/FrameAndLabel.java
+++ src/ChapterThirteen/FrameAndLabel.java
@@ -1,4 +1,26 @@
 package ChapterThirteen;
 
+import javax.swing.*;
+
+class MyFrame {
+
+    MyFrame(){
+
+        JFrame frm = new JFrame(" Window with lable");
+        frm.setBounds(400, 300, 180, 100);
+
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel lbl = new JLabel(" Hello to everybody!");
+
+        frm.add(lbl);
+    }
+}
+
 public class FrameAndLabel {
+
+    public static void main(String[] args) {
+
+        new MyFrame();
+    }
 }
