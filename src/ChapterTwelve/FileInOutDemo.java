274e29a8a68d62f9e9366cb31cd5a548775db583
Tzeentche <ilyasugako87@gmail.com>
2019-08-06 20:17:00 +0300

    Finished developing decision for sample Buffered Input / Output stream (Vasiliev, chapter 12)

--- src/ChapterTwelve/FileInOutDemo.java
+++ src/ChapterTwelve/FileInOutDemo.java
@@ -4,5 +4,34 @@ import java.io.*;
 
 public class FileInOutDemo {
 
-    
+    public static void main(String[] args) throws IOException {
+
+        try {
+
+            FileInputStream fis = new FileInputStream("D:/Books/Files/base.txt");
+
+            FileOutputStream fos = new FileOutputStream("D:/Books/Files/data.txt", true);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis, "windows-1251"));
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+            String str;
+
+            do {
+                str = br.readLine();
+                System.out.println(str);
+
+                bw.newLine();
+
+                bw.write(str.toLowerCase().replace(' ','_'));
+            } while (!str.equalsIgnoreCase("Omar Chayam"));
+
+            br.close();
+            bw.close();
+        } catch (FileNotFoundException e) {
+
+            System.out.println("File not found! " + e);
+        }
+    }
 }
