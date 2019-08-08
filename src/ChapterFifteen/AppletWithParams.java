3eea1156d0e89294d3e433a5cb407585fa4ccb23
Tzeentche <ilyasugako87@gmail.com>
2019-08-08 21:40:37 +0300

    Finished developing decision for sample Applet With Parametrs on Java (Vasiliev, chapter 15)

--- src/ChapterFifteen/AppletWithParams.java
+++ src/ChapterFifteen/AppletWithParams.java
@@ -1,4 +1,39 @@
 package ChapterFifteen;
 
-public class AppletWithParams {
+import java.awt.*;
+import javax.swing.*;
+
+public class AppletWithParams extends JApplet {
+
+    public void init() {
+
+        String text = "<html> Using font size ";
+
+        int size = Integer.parseInt(getParameter("size"));
+
+        Font fnt = new Font("Arial", Font.BOLD, size);
+
+        text += size + " .<br>";
+
+        Color clr;
+
+        String clrName = getParameter("color");
+
+        if(clrName.equalsIgnoreCase("blue")) clr = Color.BLUE;
+
+        else if(clrName.equalsIgnoreCase("red")) clr = Color.RED;
+        else if(clrName.equalsIgnoreCase("yellow")) clr = Color.YELLOW;
+        else if(clrName.equalsIgnoreCase("green")) clr = Color.GREEN;
+        else clr = Color.GRAY;
+
+        getContentPane().setBackground(clr);
+
+        text += "Font color: " + clrName.toLowerCase() + ".</html>";
+
+        JLabel lb = new JLabel(text, JLabel.CENTER);
+
+        lb.setFont(fnt);
+
+        add(lb);
+    }
 }
